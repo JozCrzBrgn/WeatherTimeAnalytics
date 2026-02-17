8162e446e1b8237b5dfe8f600ccc52b42d2c7698
Josue Cruz Barragan <ing.cruzbarragan@hotmail.com>
2026-02-16 21:03:40 -0600

    [upd] se agrega try-except y funcion connect_to_db() a main

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+import logging
+
 from api.api_requests import build_weather_url, mock_fetch_data
+from api.insert_data import connect_to_db
 
 if __name__ == "__main__":
-    url = build_weather_url("New York")
-    # data = fetch_data(url)
-    data = mock_fetch_data("New York", 1)
+    conn = None
+    try:
+        url = build_weather_url("New York")
+        # data = fetch_data(url)
+        data = mock_fetch_data("New York", 1)
+        conn = connect_to_db()
+        print(conn)
+    except:
+        logging.error("ETL execution failed", exc_info=True)
+    finally:
+        if conn:
+            conn.close()
+            logging.info("Database connection closed")
