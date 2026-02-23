347a5337906c91a01f0dc98aa6957618d58b3301
Josue Cruz Barragan <ing.cruzbarragan@hotmail.com>
2026-02-23 17:16:42 -0600

    [upd] airlow is added to docker-compose.yml

--- database/init.sql
+++ database/init.sql
@@ -5,3 +5,11 @@ CREATE DATABASE db_weather;
 \c db_weather;
 GRANT ALL PRIVILEGES ON DATABASE db_weather TO db_weather_user;
 GRANT ALL PRIVILEGES ON SCHEMA public TO db_weather_user;
+
+-- Create a database for Airflow metadata
+CREATE DATABASE airflow_db;
+
+-- Connect to airflow_db and grant permissions
+\c airflow_db;
+GRANT ALL PRIVILEGES ON DATABASE airflow_db TO db_weather_user;
+GRANT ALL PRIVILEGES ON SCHEMA public TO db_weather_user;
