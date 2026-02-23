ac4b8a2a398436d6d48778f19a52cf4dc3bea1e0
Josue Cruz Barragan <ing.cruzbarragan@hotmail.com>
2026-02-23 17:18:26 -0600

    [add] The task to insert data from airflow is created.

---
+++ airflow/dags/orchestrator.py
@@ -0,0 +1,29 @@
+import sys
+from datetime import datetime
+
+from airflow.operators.python import PythonOperator
+from airflow.providers.standard.operators.empty import EmptyOperator
+
+from airflow import DAG
+
+sys.path.append('/opt/airflow')
+from api.insert_data import main
+
+with DAG(
+    dag_id="weather-orchestrator",
+    start_date=datetime(2024, 1, 1),
+    schedule="@daily",
+    catchup=False,
+    tags=["etl", "weather"],
+) as dag:
+    
+    start = EmptyOperator(task_id='start')
+
+    insert_weather_data = PythonOperator(
+        task_id="insert_weather_data",
+        python_callable=main,
+    )
+
+    end = EmptyOperator(task_id='end')
+
+    start >> insert_weather_data >> end
